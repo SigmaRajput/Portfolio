870d030b1c4361ecbf0bec104a7adb5ce9332721
SigmaRajput <Ravisinghrajputana9@gmail.com>
2026-09-08 23:22:10 +0530

    Updates

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
-
-// https://vite.dev/config/
-export default defineConfig({
-  // Change this to match your GitHub Pages repo name, e.g. '/Portfolio/'
-  base: "/Portfolio/",
-  plugins: [react()],
-})
